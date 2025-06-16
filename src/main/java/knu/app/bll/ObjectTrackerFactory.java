3a4dca224918d74b99dc280c7d5c117abebb049f
bedu <vanntno@gmail.com>
2025-06-16 15:46:55 +0300

    Trackers Factory for quick creation trackers in real time video

---
+++ src/main/java/knu/app/bll/ObjectTrackerFactory.java
@@ -0,0 +1,35 @@
+package knu.app.bll;
+
+import knu.app.bll.processors.tracker.ObjectTracker;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class ObjectTrackerFactory {
+    private final Map<String, Supplier<ObjectTracker>> registry = new HashMap<>();
+    private static final ObjectTrackerFactory instance = new ObjectTrackerFactory();
+
+    public static ObjectTrackerFactory getInstance() {
+        return instance;
+    }
+
+    private ObjectTrackerFactory() {
+    }
+
+    public void registry(String key, Supplier<ObjectTracker> supplier) {
+        registry.put(key, supplier);
+    }
+
+    public ObjectTracker create(String key){
+        Supplier<ObjectTracker> obj = registry.get(key);
+        if (obj == null) {
+            throw new IllegalArgumentException("Unknown type: " + key);
+        }
+        return obj.get();
+    }
+
+    public enum TrackerType{
+        CSRT, MIL, KALMAN, OPTICALFLOW;
+    }
+}
