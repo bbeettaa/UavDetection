73d08182bad0c7a97d59d237ea76fca57de3b42d
bedu <vanntno@gmail.com>
2025-06-16 15:52:02 +0300

    Statistic Module

--- src/main/java/knu/app/ui/modules/StatisticDisplayUI.java
+++ src/main/java/knu/app/ui/modules/StatisticDisplayUI.java
@@ -5,11 +5,27 @@ import imgui.flag.ImGuiCond;
 import imgui.flag.ImGuiWindowFlags;
 import imgui.type.ImBoolean;
 import knu.app.bll.utils.LocalizationManager;
+import knu.app.bll.utils.SystemStats;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
 
 public class StatisticDisplayUI implements UIModule<Void> {
     private final ImBoolean windowOpen = new ImBoolean(false);
+    private static final StatisticDisplayUI ent = new StatisticDisplayUI();
 
-    public StatisticDisplayUI() {
+    private double fps = 0;
+    long frameCount = 0;
+    SystemStats stats = new SystemStats();
+
+    private long lastTime = System.nanoTime();
+    long currentTime;
+
+    public static StatisticDisplayUI getEntity() {
+        return ent;
+    }
+
+    private StatisticDisplayUI() {
     }
 
     @Override
@@ -42,14 +58,36 @@ public class StatisticDisplayUI implements UIModule<Void> {
     public void render() {
         if (!windowOpen.get()) return;
 
-        ImGui.setNextWindowPos(0, 18, ImGuiCond.Always);
+
+        ImGui.setNextWindowPos(ImGui.getWindowSizeX() - 70, 18 * 3, ImGuiCond.Always);
         ImGui.setNextWindowBgAlpha(0.35f);
         ImGui.begin(LocalizationManager.tr("statistic.menu.name"), windowOpen,
                 ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.AlwaysAutoResize);
+
         ImGui.text(String.format("%s: %.0f", LocalizationManager.tr("statistic.framerate.name"), ImGui.getIO().getFramerate()));
+        ImGui.text(String.format("%s: %.0f", LocalizationManager.tr("statistic.fps.name"), fps));
+        ImGui.text(String.format("CPU: %.1f%%", stats.getCpuLoadPercent()));
+        ImGui.text(String.format("RAM: %d / %d MB", stats.getUsedMemoryMB(), stats.getTotalMemoryMB()));
+
         ImGui.end();
 
     }
+
+
+    public void setCurrentTime() {
+        currentTime = System.nanoTime();
+    }
+
+    public void processFPS() {
+        frameCount++;
+        double elapsedTime = (currentTime - lastTime) / 1_000_000_000.0;
+        if (elapsedTime >= 1.0) {
+            fps = frameCount / elapsedTime;
+            lastTime = currentTime;
+            frameCount = 0;
+        }
+    }
+
 
 
 }
